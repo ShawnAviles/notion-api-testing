2de25e76ce15ec8d86656b57d8bb63b80d5b2db9
Shawn Aviles <shawnaviles26@gmail.com>
2022-05-21 14:49:00 -0400

    cleaned up and added axios

--- main.js
+++ main.js
@@ -1,54 +1,17 @@
-// Shawn Aviles
 // Notion API function testing
 // Note: testing functions to incorporate into personal project
 const { Client } = require("@notionhq/client");
-const fetch = require('node-fetch');
-//require('dotenv').config();
+require('dotenv').config();
 
-// Initializing CLient
+// only needed for making API requests using Axios
+const axios = require('axios');
+
+// only needed when using javascript sdk tools 
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
-/* ADD PAGE TO TABLE DATABASE */
-// ;(async () => {
-//     const response = await notion.pages.create({
-//       parent: {
-//         database_id: process.env.DATABASE_ID,
-//       },
-//       properties: {
-//         'Grocery item': {
-//           type: 'title',
-//           title: [
-//             {
-//               type: 'text',
-//               text: {
-//                 content: 'Milk',
-//               },
-//             },
-//           ],
-//         },
-//         Ran: {
-//           number: 6.99,
-//         },
-//         status : {
-//           select : {
-//             name : "Current"
-//           }
-//         },
-//         Price: {
-//           number: 4.99,
-//         },
-//         'Last ordered': {
-//           date: {
-//             start: '2022-03-12',
-//           },
-//         },
-//       },
-//     });
-//     console.log(response);
-//   })();
-
-/* JAVASCIPRT NOTION SDK TOOLS */
-const pageCreate = async () => {
+/* ADD PAGE TO (TABLE) DATABASE */
+// Using javascript SDK tools to create POST Request
+const pageCreateSDK = async (text, date) => {
   const response = await notion.pages.create({
     parent: {
       database_id: process.env.DATABASE_ID_BOARD,
@@ -59,7 +22,7 @@ const pageCreate = async () => {
           {
             type: 'text',
             text: {
-              content: 'Creating Page Test Title',
+              content: `${date}`,
             },
           },
         ],
@@ -80,7 +43,7 @@ const pageCreate = async () => {
             {
               type: 'text',
               text: {
-                content: 'this is a test for a New Card. Hopefully this works'
+                content: `${text}`
               },
             },
           ],
@@ -91,33 +54,65 @@ const pageCreate = async () => {
   return response.id;
 };
 
-
-const updatePage = async (id) => {
-  const pageID = id;
-  const updateResponse = await notion.pages.update({
-    page_id: pageID,
-    properties: {
-      Status: {
-        select: {
-          name: "Archived"
-        }
-      }
+// Using Axios to create POST Request
+const pageCreateAxios = async (text, date) => {
+  const token = process.env.NOTION_TOKEN;
+  const options = {
+    method: 'POST',
+    url: 'https://cors-anywhere.herokuapp.com/https://api.notion.com/v1/pages',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+      'Notion-Version': '2022-02-22',
+      'Content-Type': 'application/json'
+    },
+    data: {
+      parent: {
+        database_id: process.env.DATABASE_ID_BOARD,
+      },
+      properties: {
+        'Name': {
+          title: [
+            {
+              type: 'text',
+              text: {
+                content: `${date}`,
+              },
+            },
+          ],
+        },
+        Status: {
+          select : {
+            name : "Live"
+          }
+        },
+      },
+      children : [
+        {
+          object: 'block',
+          type: 'paragraph',
+          paragraph: {
+            rich_text: [
+              {
+                type: 'text',
+                text: {
+                  content: `${text}`
+                },
+              },
+            ],
+          },
+        },
+      ],
     }
-  });
-  console.log(updateResponse);
-};
+  };
+  const res = await axios.request(options);
+  return res.data.id
+}
 
 
-const retrieveDatabase = async () => {
-  const response = await notion.databases.retrieve({
-    database_id: process.env.DATABASE_ID_BOARD,
-  });
-  console.log(response);
-};
-
-
-/* JAVASCRIPT FETCH LIBRARY */
-const uppatePageFetch = async (id) => {
+/* UPDATE PAGE TO (TABLE) DATABASE */
+// Using built-in fetch to make PATCH request
+const updatePageFetch = async (id) => {
   const options = {
     method: 'PATCH',
     headers: {
@@ -141,10 +136,41 @@ const uppatePageFetch = async (id) => {
   console.log(response);
 }
 
-// updatePageFetch(process.env.DATABASE_ID_BOARD)
-// .then(response => response.json())
-// .then(response => console.log(response))
-// .catch(err => console.error(err));
+/* 
+API ISSUE USING JS SDK #1 - 
+  This was an attempted use of Javascript SDK for Updating a Page (PATCH)
+  Problem - API Request would be too large when sent on a localhost development server when updating a select property.
+    However, it was successful when being sent from this test file. Hopefully, by the end of this project I'll understand this better.
+    400 error
+  Solution - Above, I used the Fetch API library to make the PATCH request instead
+    The request was signicantly smaller in size from 2.4KB (using the sdk) to 460B (using fetch) allowing it to be sent
+*/
+const updatePageSDK = async (id) => {
+  const pageID = id;
+  const updateResponse = await notion.pages.update({
+    page_id: pageID,
+    properties: {
+      Status: {
+        select: {
+          name: "Archived"
+        }
+      }
+    }
+  });
+  console.log(updateResponse);
+};
 
-const data = retrieveDatabase();
-console.log(data.then((data) => data.id));
+
+/* RETRIEVE DATA FROM (TABLE) DATABASE */
+// Using javascript sdk 
+const retrieveDatabaseID = async () => {
+  arrayID = [];
+  const dataID = process.env.DATABASE_ID_BOARD;
+  const response = await notion.databases.query({ 
+    database_id: dataID 
+  });
+  response.results.forEach(element => arrayID.push(element.id));
+  return arrayID;
+};
+
+//retrieveDatabaseID().then(arr => arr.forEach((id) => console.log(id)));
